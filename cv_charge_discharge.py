b5cf03140a6784a268666b71e47c7117a5a6a845
Rodrigo <rodrigo.lopezbaez@imec.be>
2023-01-04 19:09:52 +0100

    new class

--- cv_charge_discharge.py
+++ cv_charge_discharge.py
@@ -6,17 +6,54 @@ from lmfit import Model
 from scipy.stats import linregress
 import seaborn as sns
 
+palette = 'Blues'
+cols = ['Potential vs Li$^+$/Li (V)','Current Density (A/cm$^2$)']
+cols2 = ['Time (s)', cols[0], 'Current (A)', 'Charge (C)', 'Capacity (mAh/cm$^3$)']
 class Ec():
-    cols = ['Potential vs Li$^+$/Li (V)','', '', '','', '','Current Density (A/cm$^2$)']
     def Electrochem(path):
         files = [os.path.join(path, i)  for i in os.listdir(path) if i != 'README.txt']
         for i in files:
             if 'CV' in i:
-                cv = [os.path.join(i, j) for j in os.listdir(i)] 
-                for x in cv:
-                    df = pd.read_csv(x, names = cols, skiprows=1)
-                    print(df)
+                cv_files = [os.path.join(i, j) for j in os.listdir(i)] 
+                for x in cv_files:
+                    cv = pd.read_csv(x, names = cols, sep = ';', skiprows=1, usecols=[0, 4])
+                    # sns.scatterplot(data = cv, x = cols[0], y = cols[1], edgecolor = None, s = 3, color = 'tab:blue')
+                    # plt.show()
+                    # plt.clf()
+            elif 'delith' in i:
+                delith_files = [os.path.join(i, j) for j in os.listdir(i)]
+                Z = [[0,0],[0,0]]
+                cbar = plt.contourf(Z, levels = np.arange(0, len(delith_files) + 1, 1), cmap=palette)
+                plt.clf()
+                n = 0
+                delith = pd.DataFrame()
+                for x in delith_files:
+                    dl = pd.read_csv(x, sep = ';', names = cols2, usecols=[1,2,3,4,5], skiprows = 1)
+                    dl['Cycle'] = int(x[-6:-4])
+                    delith = pd.concat([delith, dl])
+                    n += 1
+                sns.lineplot(data = delith, x = cols2[4], y = cols2[1], hue = 'Cycle', palette=palette, legend = False)
+                plt.colorbar(cbar).set_label('Cycle')
+                plt.show()
+                plt.clf()
                 
+            elif '\lith' in i:
+                lith_files = [os.path.join(i, j) for j in os.listdir(i)]
+                Z = [[0,0],[0,0]]
+                cbar = plt.contourf(Z, levels = np.arange(0, len(delith_files) + 1, 1), cmap=palette)
+                plt.clf()
+                n = 0
+                lith = pd.DataFrame()
+                for x in lith_files:
+                    l = pd.read_csv(x, sep = ';', names = cols2, usecols=[1,2,3,4,5], skiprows = 1)
+                    l['Cycle'] = int(x[-6:-4])
+                    lith = pd.concat([lith, l])
+                    n += 1
+                sns.lineplot(data = lith, x = cols2[4], y = cols2[1], hue = 'Cycle', palette=palette, legend = False)
+                plt.colorbar(cbar).set_label('Cycle')
+                plt.show()
 
+
+sns.set_palette(palette)
 path = r'C:\Users\lopezb41\OneDrive - imec\Documents\Experiments\Data\Electrochemical\LSB_09\0I_07'
 Ec.Electrochem(path)
