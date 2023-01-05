1c4adc7f12c4f735d0b295519f178368058b2e50
Rodrigo <rodrigo.lopezbaez@imec.be>
2023-01-05 10:13:10 +0100

    update

--- cv_charge_discharge.py
+++ cv_charge_discharge.py
@@ -7,6 +7,7 @@ from scipy.stats import linregress
 import seaborn as sns
 
 palette = 'Blues'
+dpi = 100
 cols = ['Potential vs Li$^+$/Li (V)','Current Density (A/cm$^2$)']
 cols2 = ['Time (s)', cols[0], 'Current (A)', 'Charge (C)', 'Capacity (mAh/cm$^3$)']
 class Ec():
@@ -17,9 +18,8 @@ class Ec():
                 cv_files = [os.path.join(i, j) for j in os.listdir(i)] 
                 for x in cv_files:
                     cv = pd.read_csv(x, names = cols, sep = ';', skiprows=1, usecols=[0, 4])
+                    fig, ax = plt.subplots(facecolor = 'white', dpi = dpi)
                     sns.scatterplot(data = cv, x = cols[0], y = cols[1], edgecolor = None, s = 3, color = 'tab:blue')
-                    plt.show()
-                    plt.clf()
             elif 'delith' in i:
                 delith_files = [os.path.join(i, j) for j in os.listdir(i)]
                 Z = [[0,0],[0,0]]
@@ -32,10 +32,9 @@ class Ec():
                     dl['Cycle'] = int(x[-6:-4])
                     delith = pd.concat([delith, dl])
                     n += 1
+                fig, ax = plt.subplots(facecolor = 'white', dpi = dpi)
                 sns.lineplot(data = delith, x = cols2[4], y = cols2[1], hue = 'Cycle', palette=palette, legend = False)
                 plt.colorbar(cbar).set_label('Cycle')
-                plt.show()
-                plt.clf()
                 
             elif '\lith' in i:
                 lith_files = [os.path.join(i, j) for j in os.listdir(i)]
@@ -49,7 +48,6 @@ class Ec():
                     l['Cycle'] = int(x[-6:-4])
                     lith = pd.concat([lith, l])
                     n += 1
+                fig, ax = plt.subplots(facecolor = 'white', dpi = dpi)
                 sns.lineplot(data = lith, x = cols2[4], y = cols2[1], hue = 'Cycle', palette=palette, legend = False)
                 plt.colorbar(cbar).set_label('Cycle')
-                plt.show()
-                plt.clf()
