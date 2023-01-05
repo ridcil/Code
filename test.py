240f51373586041ff2b7f87dd63c60eb884213b1
Rodrigo <rodrigo.lopezbaez@imec.be>
2023-01-05 18:44:07 +0100

    xrd notebook

--- test.py
+++ test.py
@@ -12,10 +12,11 @@ sns.set_theme(style="darkgrid")
 
 path = r'C:\Users\lopezb41\OneDrive - imec\Documents\Experiments\Data\XRD\LSB_07\HS33_02\Data\HS33_02.csv'
 cols = ['Angle', 'Intensity']
-print(cols)
-# df = pd.read_csv(path, skiprows = 30, usecols=[0, 2])
+df = pd.read_csv(path, skiprows = 30, usecols=[0, 2])
+x, y = np.array(df[cols[0]]), np.array(df[cols[1]])
+data = xrd.Chart(x, y)
 
-# x, y = np.array(df[cols[0]]), np.array(df[cols[1]])
-# data = xrd.Chart(x, y)
-# plt.plot(data)
+
+plt.plot(data)
+plt.show()
 # print(df)
