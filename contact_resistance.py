8ee305db17486c7b0fa62e79a4af0672ae3979c7
Rodrigo <rodrigo.lopezbaez@imec.be>
2023-02-02 15:07:33 +0100

    update

--- contact_resistance.py
+++ contact_resistance.py
@@ -6,20 +6,18 @@ from lmfit import Model
 from scipy.stats import linregress
 import seaborn as sns
 
-class Fit():
-    def files(path):
-        return [os.path.join(path, i) for i in os.listdir(path)]
+def r_t(s, r_s, l_t, N, W, L):
+    return (r_s * s / (N * W + (N-1) * (2 *s + L))) + (2 * r_s * l_t / (N * W + (N-1) * (2 *s + L)))
+gmodel5 = Model(r_t)
+param5 = gmodel5.make_params()
+param5['r_s'].set(value = 1e8, min = 0)
+param5['l_t'].set(value = 1, min = 0)
+param5['L'].set(value = 3200e-4, vary = False)
+param5['W'].set(value = 5e-4, vary = False)
+param5['N'].set(value = 60, vary = False)
 
-    def r_t(s, r_s, l_t, N, W, L):
-        return (r_s * s / (N * W + (N-1) * (2 *s + L))) + (2 * r_s * l_t / (N * W + (N-1) * (2 *s + L)))
-    gmodel5 = Model(r_t)
-    param5 = gmodel5.make_params()
-    param5['r_s'].set(value = 1e8, min = 0)
-    param5['l_t'].set(value = 1e-5, min = 1e-9)
-    param5['L'].set(value = 3200e-4, vary = False)
-    param5['W'].set(value = 5e-4, vary = False)
-    param5['N'].set(value = 60, vary = False)
-    
+class Fit():
+        
     def r_fit(path, columns, sample_name):
         files = [os.path.join(path, i) for i in os.listdir(path)]
         cols = ['Spacing (cm)', 'Resistance ($\Omega$)']
